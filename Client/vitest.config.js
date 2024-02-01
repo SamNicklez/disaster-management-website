e5db37f947edbc228b65d7614add9ca09212c9a2
SamNicklez <snicklaus@uiowa.edu>
2024-02-01 15:47:43 -0600

    Added vitest testing

--- Client/vitest.config.js
+++ Client/vitest.config.js
@@ -9,8 +9,8 @@ export default mergeConfig(
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/*'],
       root: fileURLToPath(new URL('./', import.meta.url)),
-      reporters: 'junit',
-      outputFile: 'test-results.xml' // You can specify a different file name or path if neede
+      reporters: 'vitest-sonar-reporter',
+      outputFile: 'test-results.xml', // You can specify a different file name or path if neede
     }
   })
 )
