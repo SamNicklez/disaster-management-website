3d3cbe0be8fbed86f922f0ed21f269c2f32d3ea3
SamNicklez <snicklaus@uiowa.edu>
2024-02-06 13:15:47 -0600

    Added alert in login to prompt user

--- Client/src/components/LoginComponent.vue
+++ Client/src/components/LoginComponent.vue
@@ -5,17 +5,19 @@ export default {
         visible: false,
         username: '',
         password: '',
+        alertText: 'This is a warning alert',
+        alert: false,
     }),
     methods: {
         goToSignUp() {
             this.$router.push({ name: 'signup' });
         },
-        login(){
+        login() {
             console.log('Logging in');
             console.log(this.username);
             console.log(this.password);
         },
-        forgotPass(){
+        forgotPass() {
             console.log('Forgot password');
             //Router push to somewhere to reset password
         }
@@ -25,22 +27,27 @@ export default {
 
 <template>
     <div class="login-card">
+
         <v-card class="mx-auto pa-12 pb-8" id="card" elevation="8" rounded="lg" style="margin-bottom: 10vh;">
             <div class="text-h5 text-center mb-8">Login</div>
+            <v-alert v-model="alert" density="compact" variant="outlined" type="warning"
+                :text="alertText" dismissible><v-icon icon="mdi-close-box-outline" size="large" style="float:right" @click="this.alert=false"></v-icon></v-alert>
             <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-            <v-text-field density="compact" v-model="username" maxLength="15" placeholder="Username" prepend-inner-icon="mdi-account-circle"
-                variant="outlined"></v-text-field>
+            <v-text-field density="compact" v-model="username" maxLength="15" placeholder="Username"
+                prepend-inner-icon="mdi-account-circle" variant="outlined"></v-text-field>
             <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                 Password
                 <a class="text-caption text-decoration-none" style="color:#313638; cursor: pointer;" @click="forgotPass">
                     Forgot login password?</a>
             </div>
             <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-                density="compact" v-model="password" placeholder="Enter your password" maxLength="15" prepend-inner-icon="mdi-lock-outline" variant="outlined"
+                density="compact" v-model="password" placeholder="Enter your password" maxLength="15"
+                prepend-inner-icon="mdi-lock-outline" variant="outlined"
                 @click:append-inner="visible = !visible"></v-text-field>
             <v-card class="mb-12" color="#313638" variant="tonal">
                 <v-card-text class="text-medium-emphasis text-caption">
-                    Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for 10 minutes. If you must login now, you can also click "Forgot login password?" below to reset the login
+                    Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for 10
+                    minutes. If you must login now, you can also click "Forgot login password?" below to reset the login
                     password.
                 </v-card-text>
             </v-card>
@@ -57,7 +64,7 @@ export default {
 </template>
 
 <style scoped>
-#card{
+#card {
     width: 35em;
 }
 </style>
