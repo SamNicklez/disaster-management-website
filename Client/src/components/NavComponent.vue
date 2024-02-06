5c70527a0817e1198451fe569e6b43fa3fa893ca
SamNicklez <snicklaus@uiowa.edu>
2024-02-06 11:27:46 -0600

    added notification drop down in navbar

--- Client/src/components/NavComponent.vue
+++ Client/src/components/NavComponent.vue
@@ -4,6 +4,12 @@ export default {
   data() {
     return {
       searchQuery: '',
+      items: [
+        { title: 'Notification 1', description: 'This is a notification' },
+        { title: 'Notification 2', description: 'This is a notification' },
+        { title: 'Notification 3', description: 'This is a notification' },
+        { title: 'Notification 4', description: 'This is a notification' },
+      ],
     };
   },
   methods: {
@@ -23,9 +29,9 @@ export default {
 <template>
   <v-container>
     <v-app-bar scroll-behavior="hide">
-        <v-toolbar-title>
-          <v-btn text to="/" variant="plain" class="button">Disaster Donation</v-btn>
-        </v-toolbar-title>
+      <v-toolbar-title>
+        <v-btn text to="/" variant="plain" class="button">Disaster Donation</v-btn>
+      </v-toolbar-title>
       <v-spacer></v-spacer>
       <div class="nav-textfield">
         <v-text-field v-model="searchQuery" dense flat hide-details prepend-icon="mdi-magnify"
@@ -36,8 +42,21 @@ export default {
       <v-spacer></v-spacer>
       <v-btn @click="openLogin">Login</v-btn>
       <v-btn icon @click="openProfile">
-        <v-icon>mdi-account-circle</v-icon>
+        <v-icon size="large">mdi-account-circle</v-icon>
       </v-btn>
+      <v-menu transition="scale-transition">
+        <template v-slot:activator="{ props }">
+          <v-btn icon @click="populateNotifications" v-bind="props">
+            <v-icon size="large">mdi-bell</v-icon>
+          </v-btn>
+        </template>
+
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i">
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
   </v-container>
 </template>
@@ -46,14 +65,16 @@ export default {
 .nav-textfield {
   width: 35%;
   margin: 0 auto;
-  color:black;
+  color: black;
 }
-.button{
+
+.button {
   color: black;
   font-size: 0.75em;
   margin-left: 0.5vw;
 }
-.button:hover{
+
+.button:hover {
   color: black;
 }
 </style>
