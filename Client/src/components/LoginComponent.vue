748b22035a3c87b6796a3117918d86dae147da40
SamNicklez <snicklaus@uiowa.edu>
2024-02-06 08:27:41 -0600

    added character length to username and password

--- Client/src/components/LoginComponent.vue
+++ Client/src/components/LoginComponent.vue
@@ -3,10 +3,17 @@ import '@mdi/font/css/materialdesignicons.css'
 export default {
     data: () => ({
         visible: false,
+        username: '',
+        password: '',
     }),
     methods: {
         goToSignUp() {
-            this.$router.push({ name: 'signup' }); // Assume your sign-up route is named 'signup'
+            this.$router.push({ name: 'signup' });
+        },
+        login(){
+            console.log('Logging in');
+            console.log(this.username);
+            console.log(this.password);
         },
     },
 };
@@ -16,7 +23,7 @@ export default {
     <div>
         <v-card title="Login" class="mx-auto pa-12 pb-8" id="card" elevation="8" rounded="lg" style="margin-bottom: 10vh;">
             <div class="text-subtitle-1 text-medium-emphasis">Account</div>
-            <v-text-field density="compact" placeholder="Username" prepend-inner-icon="mdi-account-circle"
+            <v-text-field density="compact" v-model="username" maxLength="15" placeholder="Username" prepend-inner-icon="mdi-account-circle"
                 variant="outlined"></v-text-field>
             <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
                 Password
@@ -24,7 +31,7 @@ export default {
                     Forgot login password?</a>
             </div>
             <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-                density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline" variant="outlined"
+                density="compact" v-model="password" placeholder="Enter your password" maxLength="15" prepend-inner-icon="mdi-lock-outline" variant="outlined"
                 @click:append-inner="visible = !visible"></v-text-field>
             <v-card class="mb-12" color="surface-variant" variant="tonal">
                 <v-card-text class="text-medium-emphasis text-caption">
@@ -33,7 +40,7 @@ export default {
                     password.
                 </v-card-text>
             </v-card>
-            <v-btn block class="mb-8" color="blue" size="large" variant="tonal">
+            <v-btn block class="mb-8" color="blue" size="large" variant="tonal" @click="login">
                 Log In
             </v-btn>
             <v-card-text class="text-center">
