224d185d57c7cc7ccec1d1cb79d7952344fc3f81
SamNicklez <snicklaus@uiowa.edu>
2024-02-20 09:01:23 -0600

    started signup route

--- Server/routes/Users.py
+++ Server/routes/Users.py
@@ -30,3 +30,41 @@ def signin():
     except Exception as e:
         print(f"Error: {str(e)}")
         return jsonify({"error": "Internal Server Error"}), 500
+    
+@users_bp.route('/signup', methods=['POST'])
+def signup():
+    try:
+    # Users table
+    # UserId: int = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # Username: str = db.Column(db.String(45), nullable=False)
+    # FirstName: str = db.Column(db.String(45), nullable=False)
+    # LastName: str = db.Column(db.String(45), nullable=False)
+    # email: str = db.Column(db.String(255), nullable=False)
+    # PhoneNumber: int = db.Column(db.Integer)
+    # Password: str = db.Column(db.String(255), nullable=False)
+    # RoleID: int = db.Column(db.Integer, db.ForeignKey('roles.RoleID'))
+    # IsVerified: int = db.Column(db.Integer)
+    # ZipCode: int = db.Column(db.Integer, nullable=False)
+        
+    # Example Create 
+    # new_item = Item(ItemName=data['ItemName'], CategoryId=data['CategoryId'], ItemDescription = data['ItemDescription'])
+    # db.session.add(new_item)
+    # db.session.commit()
+    # return ('success')
+    
+    # except Exception as e:
+    #     print(f"Error: {str(e)}")
+    #     return jsonify({"error": "Internal Server Error"}), 500
+        
+        data = request.get_json()
+        
+        hashed_password = generate_password_hash(data["password"], method="sha256")
+        new_user = User(UserID=data["UserID"], password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        
+        return jsonify({"message": "User created successfully"}), 201
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return jsonify({"error": "Internal Server Error"}), 500
