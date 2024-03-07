f97c53fd787fd3b6f8178470e0020f5038e6f45d
SamNicklez <snicklaus@uiowa.edu>
2024-03-07 11:53:30 -0600

    Added event management page routing

---
+++ Client/src/views/EventManagement.vue
@@ -0,0 +1,24 @@
+<script setup></script>
+
+<template>
+  <div class="card">
+    Event page
+    <br />
+
+    <br />
+    <v-btn to="/">Home</v-btn>
+    <br />
+    <v-btn to="/login">Login</v-btn>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  background-color: #990000;
+  width: 100%;
+  height: 50%;
+  text-align: center;
+  color: white;
+  font-size: 2em;
+}
+</style>
