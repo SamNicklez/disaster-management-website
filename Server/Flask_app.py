68fb4791c7f7126604fe34fdd0728889146fc085
uditha <udithaj7@gmail.com>
2024-03-12 23:06:47 -0500

    Flask app.py changes

--- Server/Flask_app.py
+++ Server/Flask_app.py
@@ -3,18 +3,25 @@ from flask_cors import CORS
 from stores import db
 from routes.Items import items_bp
 from routes.Users import users_bp
+from routes.Events import events_bp
 import os
 
 app = Flask(__name__)
-CORS(app)
-app.config['SQLALCHEMY_DATABASE_URI'] =f'mysql://root:{os.getenv("DB_PASSWORD")}@localhost:3306/theapp'
+# CORS(app)
+# app.config['SQLALCHEMY_DATABASE_URI'] =f'mysql://root:{os.getenv("DB_PASSWORD")}@localhost:3306/theapp'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.secret_key = os.getenv("SECRET_KEY")
+# db.init_app(app)
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] ='mysql://root:newSQLIowa00@localhost:3306/theapp'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = os.getenv("SECRET_KEY")
 db.init_app(app)
 
 app.register_blueprint(users_bp, url_prefix='/users_bp')
 app.register_blueprint(items_bp, url_prefix='/item')
-
+app.register_blueprint(events_bp, url_prefix='/event')
 
 @app.route("/")
 def home():
