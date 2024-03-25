5957e17302cada5092a98ded8965a7ecca515f64
SamNicklez <snicklaus@uiowa.edu>
2024-03-25 17:58:30 -0500

    Added pledges model

---
+++ Server/models/Pledges.py
@@ -0,0 +1,10 @@
+from stores import db
+
+class Category(db.Model):
+    __tablename__ = 'pledges'
+    pledge_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    item_id = db.Column(db.Integer, nullable=False)
+    quantity_given = db.Column(db.Integer, nullable=False)
+    quantity_received = db.Column(db.Integer, nullable=False, default=0)
+    is_fulfilled = db.Column(db.Integer, nullable=False, default=0)
